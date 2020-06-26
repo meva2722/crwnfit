fad4e093158eca0076196ddcd71a126b9f843d50
Marcus Evans <meva2722@students.vsu.edu>
2020-06-26 00:26:01 -0400

    Initial Commit

---
+++ Crown FitnessTests/Crown_FitnessTests.swift
@@ -0,0 +1,34 @@
+//
+//  Crown_FitnessTests.swift
+//  Crown FitnessTests
+//
+//  Created by Marcus Evans on 6/26/20.
+//  Copyright © 2020 Marcus Evans. All rights reserved.
+//
+
+import XCTest
+@testable import Crown_Fitness
+
+class Crown_FitnessTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
